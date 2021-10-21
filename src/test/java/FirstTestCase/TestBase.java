a5bf525073e36f3d26ad1d8336dc219fd8158fb1
ZergEmpire <71401820+ZergEmpire@users.noreply.github.com>
2021-10-21 16:57:28 +0300

    Update TestBase.java

--- src/test/java/FirstTestCase/TestBase.java
+++ src/test/java/FirstTestCase/TestBase.java
@@ -64,7 +64,7 @@ public class TestBase {
 
         WebDriverRunner.setWebDriver(driver);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
 
